843686aae20132f228966a7670abdbc847be576d
nGreg <nGreg72@gmail.com>
2021-03-03 09:16:06 +0900

    Trying get list dir.

--- output week 1/test.py
+++ output week 1/test.py
@@ -1,6 +1,25 @@
-a = [i for i in range(1001)]
+import os
 
-for i in a:
-    i = str(i)
-    if i.split(i[-1]) == 2:
-        print(i)
+# def startRename():
+#     directory = f"{os.getcwd()}\\"
+#     directory1 = directory.replace('/', '\\')
+#     files = os.listdir(directory)
+#
+#     for file in files:
+#         os.rename(directory1 + file, directory1 + file.replace('Без названия', ''))
+#
+# startRename()
+
+baseDir = 'C:/Temp/'
+# bd = filter(os.path.isdir, os.listdir(baseDir))
+
+directories = filter(os.path.isdir, os.listdir(baseDir))
+for d in directories:
+    print(d)
+
+# for ls in directories:
+
+    # if os.path.isdir(fn):
+    #     print()
+        # continue
+    # else:
