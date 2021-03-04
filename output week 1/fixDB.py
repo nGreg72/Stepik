5fbb50b7164cf9cf1f25f21315694d47005e4d58
nGreg72 <ngerg72@gmail.com>
2021-03-04 14:23:29 +0900

    Regexp testing.

--- output week 1/fixDB.py
+++ output week 1/fixDB.py
@@ -1,12 +1,13 @@
 import re
 
-title = "utro_YKT_140121-NID130770-4869434.avi"
-title_lrv = "utro_YKT_140121-NID130770-4869434.11898/utro_YKT_140121-NID130770-4869434_lrv.avi"
-
-file = open("store_d.txt", "r")
+file = open("store_d.txt", "r+")
 lines = file.readlines()
 
-for ln in lines:
-    print(ln)
+pattern = "-NID.+"
+
+for ln in lines :
+    if "title" and "NID" in ln:
+        re.sub(pattern, '', ln)
+        print(ln)
 
 file.close()
