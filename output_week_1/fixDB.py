669bbff55d97956812c8c7d9785655713dd411e0
greg <ngreg72@gmail.com>
2021-03-09 09:20:24 +0900

    fixDB GUI added.

--- output_week_1/fixDB.py
+++ output_week_1/fixDB.py
@@ -1,19 +1,22 @@
+from tkinter.filedialog import askopenfilename, asksaveasfile
 import re
 
+readfile = askopenfilename()
+writefile = asksaveasfile()
+
 with open("new_store_d.txt", "a", encoding="utf-8") as new_file:
-    file = open("store_d.txt", "r", encoding="utf-8")
+    file = open(readfile, "r", encoding="utf-8")
     lines = file.readlines()
 
     pattern = "-NID.+"
 
     for ln in lines:
         if "\"title\"" in ln and "NID" in ln:
-
             tmp = re.sub(pattern, '\"', ln)
-            # print(ln)
-            # print(tmp)
-            new_file.write(tmp)
-        else:
-            new_file.write(ln)
+            print(ln)
+            print(tmp)
+        #     new_file.write(tmp)
+        # else:
+        #     new_file.write(ln)
 
     file.close()
